cde9f10410bc0e5d4b8d5b4284a6342a1325f41b
Alice <alice.balard@fu-berlin.de>
2016-10-12 17:24:06 +0200

    massive changes before BeGenDiv

--- R/Align_and_Count.R
+++ R/Align_and_Count.R
@@ -1,19 +1,13 @@
-setwd("~/Ef_Bait_Capture/R/Results")
+setwd("/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_ResultsCode/")
 library(Rsubread)
 library(reshape)
 library(ggplot2)
 library(pheatmap)
-###################### IDEAS ###################### 
-#Hierarchical clustering on the readcounts per bait.  V
-#A heatmap just on this raw data?  V
-#Pairs plot to identify correlating (good) libraries.  V
-#Table of pairwise correlation coefficients.  V
-#And a truly random 120nt baits gff would be awesome ;-)
-#Just random no intron or etc selection needed.
-#My guess is the hierarchical clustering will show a cluster of "working" baits. And the correlations will show "working" libraries.
-#column scaling of the libraries might give the best clustering.
-#is there reads to non baits region in the genomes?
-################################################### 
+library(gridExtra)
+library(xtable)
+options(xtable.floating = FALSE)
+options(xtable.timestamp = "")
+######################################
 
 ## built an index (hash table) for read mapping
 if(!file.exists("/SAN/Alices_sandpit/sequencing_data_dereplicated/Efal_mtapi_reference_index.files")){
@@ -29,138 +23,112 @@ FilesR2 <- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated",
                       pattern="R2_001.fastq.unique.gz$", full.names=TRUE)
 
 ## 2919Single_S12(FilesR1[15]) was excluded, keep crashing (memory pb?), same pb with 2809Si [9] + 2812Do [11]
-FilesR1 <- FilesR1[-c(9,11,15)]
-FilesR2 <- FilesR2[-c(9,11,15)]
+FilesR1 <- FilesR1[-c(9,11,15,19)] # exclude also "undetermined"
+FilesR2 <- FilesR2[-c(9,11,15,19)]
 
-## Create the alignments ## SOME BUGS with R, ran outside ## /home/alice/subread-1.5.0-p3-source/bin/subread-align -T 16 -i Efal_mtapi_reference_index -r 2812Single_S10_R1_001.fastq.unique.gz -R 2812Single_S10_R2_001.fastq.unique.gz -I 10 -M 3 -t 1 -o 2812Single_S10_R1_001.fastq.unique.gz_3.BAM
+## Create the alignments 
 Mismatches <- c(10,20,30,50,70)
-if(file.exists("/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments/2672Single_S17_R1_001.fastq.unique.gz_BAM")){
-    sapply(Mismatches, function (MM){
-        Rsubread::align(index="/SAN/Alices_sandpit/sequencing_data_dereplicated/Efal_mtapi_reference_index",
-                      readfile1=FilesR1,
-                      readfile2=FilesR2,
-                      type="dna",
-                      maxMismatches=MM,
-                      indels=10,
-                      nthreads=length(FilesR1),
-                      output_file=paste(FilesR1, "_", MM, ".BAM", sep="")
-                      )
-})}
-
-## Create the featurecounts objects
-bams_10<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
-                  pattern="10.BAM$", full.names=TRUE)
-FC_10<- featureCounts(bams_10,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
-                    isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
-                    allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
-                    isPairedEnd=TRUE,reportReads=TRUE,nthreads=20)
-
-bams_20<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
-                  pattern="20.BAM$", full.names=TRUE)
-FC_20<- featureCounts(bams_20,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
-                    isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
-                    allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
-                    isPairedEnd=TRUE,reportReads=TRUE,nthreads=20)
-
-bams_30<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
-                  pattern="30.BAM$", full.names=TRUE)
-FC_30<- featureCounts(bams_30,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
-                    isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
-                    allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
-                    isPairedEnd=TRUE,reportReads=TRUE,nthreads=20)
-
-bams_50<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
-                  pattern="50.BAM$", full.names=TRUE)
-FC_50<- featureCounts(bams_50,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
-                    isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
-                    allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
-                    isPairedEnd=TRUE,reportReads=TRUE,nthreads=20)
-
-bams_70<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
-                  pattern="70.BAM$", full.names=TRUE)
-FC_70<- featureCounts(bams_70,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
-                    isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
-                    allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
-                    isPairedEnd=TRUE,reportReads=TRUE,nthreads=20)
-
-                                        # I. First look into the data
-
-## make a loop for all mismatches !!!!
-
-MM <- 10
-FC <-get(paste("FC_", MM, sep=""))
-
-## 1- calculate proportion assigned to baits
-assigned <- as.numeric(sapply(FC$stat, "[[", 1))
-total <- 0
-for (i in 1 : 11) {
-    t <- as.numeric(sapply(FC$stat, "[[", i))
-    total <- total +t}
-propassigned <- assigned/total*100 ; propassigned <- propassigned[-1]
-
-## 2- names of the samples
-names <- substr(FC$targets, 82,87)
-
-## 3- proportion mapped to the genome
-propomapped <- propmapped(FC)
+## sapply(Mismatches, function (MM){
+##         Rsubread::align(index="/SAN/Alices_sandpit/sequencing_data_dereplicated/Efal_mtapi_reference_index",
+##                       readfile1=FilesR1,
+##                       readfile2=FilesR2,
+##                       type="dna",
+##                       maxMismatches=MM,
+##                       indels=10,
+##                       nthreads=length(FilesR1),
+##                       output_file=paste(FilesR1, "_", MM, ".BAM", sep="")
+##                       )})
 
 
+### Create the featurecounts objects and save them as tab-delimited file which includes identifier, length and counts for each bait in each library
+
+## bams_10<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
+##                    pattern="10.BAM$", full.names=TRUE)
+## FC_10<- featureCounts(bams_10,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
+##                      isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
+##                      allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
+##                      isPairedEnd=TRUE,reportReads=TRUE)
+##write.table(x=data.frame(FC_10$annotation[,c("GeneID","Length")],FC_10$counts,stringsAsFactors=FALSE),file="counts_10.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=c("BaitID", "LengthBait", paste("Lib",substr(names(FC_10$stat[-1]),82,87), sep="_")))
+## bams_20<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
+##                   pattern="20.BAM$", full.names=TRUE)
+## FC_20<- featureCounts(bams_20,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
+##                     isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
+##                     allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
+##                     isPairedEnd=TRUE,reportReads=TRUE)
+#write.table(x=data.frame(FC_20$annotation[,c("GeneID","Length")],FC_20$counts,stringsAsFactors=FALSE),file="counts_20.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=c("BaitID", "LengthBait", paste("Lib",substr(names(FC_20$stat[-1]),82,87), sep="_")))
+## bams_30<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
+##                   pattern="30.BAM$", full.names=TRUE)
+## FC_30<- featureCounts(bams_30,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
+##                     isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
+##                     allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
+##                     isPairedEnd=TRUE,reportReads=TRUE)
+#write.table(x=data.frame(FC_30$annotation[,c("GeneID","Length")],FC_30$counts,stringsAsFactors=FALSE),file="counts_30.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=c("BaitID", "LengthBait", paste("Lib",substr(names(FC_30$stat[-1]),82,87), sep="_")))
+## bams_50<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
+##                   pattern="50.BAM$", full.names=TRUE)
+## FC_50<- featureCounts(bams_50,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
+##                     isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
+##                     allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
+##                     isPairedEnd=TRUE,reportReads=TRUE)
+#write.table(x=data.frame(FC_50$annotation[,c("GeneID","Length")],FC_50$counts,stringsAsFactors=FALSE),file="counts_50.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=c("BaitID", "LengthBait", paste("Lib",substr(names(FC_50$stat[-1]),82,87), sep="_")))
+## bams_70<- list.files(path = "/SAN/Alices_sandpit/sequencing_data_dereplicated/All_alignments_Rsubread_align/",
+##                   pattern="70.BAM$", full.names=TRUE)
+## FC_70<- featureCounts(bams_70,annot.ext="/SAN/Alices_sandpit/MYbaits_Eimeria_V1.single120_feature_counted.gtf",
+##                     isGTFAnnotationFile=TRUE, GTF.featureType = "sequence_feature", useMetaFeatures=FALSE, GTF.attrType="bait_id",
+##                     allowMultiOverlap=TRUE,## important to allow reads to map multiple baits
+##                     isPairedEnd=TRUE,reportReads=TRUE)
+#write.table(x=data.frame(FC_70$annotation[,c("GeneID","Length")],FC_70$counts,stringsAsFactors=FALSE),file="counts_70.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=c("BaitID", "LengthBait", paste("Lib",substr(names(FC_70$stat[-1]),82,87), sep="_")))
+
+## Load the tab delimited files, results from featureCounts
+counts10 <- read.table("counts_10.txt", header = TRUE);counts10 <- counts10[-ncol(counts10)]
+counts20 <- read.table("counts_20.txt", header = TRUE);counts20 <- counts20[-ncol(counts20)]
+counts30 <- read.table("counts_30.txt", header = TRUE);counts30 <- counts30[-ncol(counts30)]
+counts50 <- read.table("counts_50.txt", header = TRUE);counts50 <- counts50[-ncol(counts50)]
+counts70 <- read.table("counts_70.txt", header = TRUE);counts70 <- counts70[-ncol(counts70)]
+
+## Calculate proportion assigned to baits
+propmapped
+## To be continued.....
 
 
-## create a DF
-DF_summary <- as.data.frame(names,propassigned,propomapped)
+## For each library, get the number of efficient baits
+coverage <- 10
+l <- ncol(counts10)
+
+## How many baits capture sequences?
+myvec1 <- vector(mode="numeric", length=0);myvec2 <- vector(mode="numeric", length=0);myvec3 <- vector(mode="numeric", length=0);myvec4 <- vector(mode="numeric", length=0);myvec5 <- vector(mode="numeric", length=0)
+for (i in (3:l)) {
+    myvec1 <- c(myvec1,sum(counts10[i]>coverage))
+    myvec2 <- c(myvec2,sum(counts20[i]>coverage))
+    myvec3 <- c(myvec3,sum(counts30[i]>coverage))
+    myvec4 <- c(myvec4,sum(counts50[i]>coverage))
+    myvec5 <- c(myvec5,sum(counts70[i]>coverage))
+}
+mytab <- data.frame(names(counts10)[3:l],myvec1,myvec2,myvec3,myvec4,myvec5)
+names(mytab) <- c("Libraries", "MM=10", "MM=20", "MM=30", "MM=50", "MM=70")
+pdf("Fig_NumberEfficientBaitsCov10.pdf", height=11, width=8.5, title ="Baits with a minimum coverage of 10")
+grid.table(mytab)
+dev.off()
+## same with coverage = 1 saved in "Fig_NumberEfficientBaitsCov1.pdf"
+
+## Which baits? Selection of the efficient baits
+## To be continued.....
+GoodBaits <- counts10$BaitID[counts10[3]>coverage]
+
+## Plot a heatmap of the "cool" baits
+NamesB <- counts10[,1]
+df <- counts10[-c(1,2)]
+mymat <- as.matrix(df)
+rownames(mymat) <- NamesB
 
 
 
-                                        # II. Observe the counts of assigned baits / lib depending of wanted coverage min per bait
-countsDF <- FC$counts
-colnames(countsDF) <- paste("Lib_",substr(colnames(countsDF),82,87),sep="") # shorten names
-libraries <- 1:10 ; coverage <- 1:100 ##
-tabcountcov <- matrix(, nrow = 10, ncol = 100)
-for (l in libraries){
-    for (c in coverage){
-        tabcountcov[l,c] <- (table(rowSums(countsDF > c) > l))[2]}}
-colnames(tabcountcov) <- as.character(1:100)
-df <- melt(tabcountcov[,c(1,2,5,10,15,20,30,50,100)]); names(df) <- c("nLib","cov","nBaits")
-ggplot(data=df, aes(x=nLib, y=nBaits, group=cov)) + geom_line(aes(color=factor(cov))) + scale_color_discrete(name="Divers levels\nof coverage") + ggtitle(paste(as.character(MM),"mismatches maximum allowed in the alignment"))
+## 1 -> baits with at least a sum of coverage of 500 for all the libraries
 
-                                        # III. Plot a heatmap of the "cool" baits
-table(rowSums(countsDF)>500)
+pheatmap(mymat[rowSums(mymat)>500,])
+
+
+help(pheatmap)
+table(rowSums(counts10[3:l])>500)
 #countsDF[grepl("apico", rownames(countsDF)),]
 
 pheatmap(log10(countsDF[rowSums(countsDF)>500,]+0.1))
-##pheatmap(log10(countsDF[rowSums(countsDF)>50,]+0.1))
-##pheatmap(log10(countsDF[rowSums(countsDF)>10,]+0.1))
-
-
-                                        # Bonus
-## ## Plot correlation heatmap + pairwise coef [adapted from http://www.sthda.com/english/wiki/ggplot2-quick-correlation-matrix-heatmap-r-software-and-data-visualization]
-## create_heatmap_correl <- function(mydata){
-##     cormat <- round(cor(mydata),2)# I.Compute the correlation matrix
-##     get_upper_tri <- function(cormat){# II.Get upper triangle of the correlation matrix
-##         cormat[lower.tri(cormat)]<- NA; return(cormat)}
-##     upper_tri <- get_upper_tri(cormat)
-##     melted_cormat <- na.omit(melt(upper_tri))# II.Melt the correlation matrix
-##     ggplot(data = melted_cormat, aes(X2,X1, fill = value))+ # IV.Plot
-##         geom_tile(color = "white")+
-##         scale_fill_gradient2(low = "blue", high = "red",
-##                              mid = "white",midpoint = 0,
-##                              limit = c(-1,1), space = "Lab",
-##                              name="Pearson\nCorrelation") +
-##         theme_minimal()+
-##         theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 12, hjust = 1))+
-##         coord_fixed()+ geom_text(aes(X2, X1, label = value),
-##                                  color = "black", size = 4) +  #Add correlation coefficients on the heatmap
-##         theme(axis.title.x = element_blank(),
-##               axis.title.y = element_blank(),
-##               panel.grid.major = element_blank(),
-##               panel.border = element_blank(),
-##               panel.background = element_blank(),
-##               axis.ticks = element_blank(),
-##               legend.justification = c(1, 0),
-##               legend.position = c(0.6, 0.7),
-##               legend.direction = "horizontal")+
-##         guides(fill = guide_colorbar(barwidth = 7, barheight = 1,title.position = "top", title.hjust = 0.5)) }
-
-## create_heatmap_correl(countsDF[!grepl("apico|mito", rownames(countsDF)),])
-##create_heatmap_correl(countsDF)
